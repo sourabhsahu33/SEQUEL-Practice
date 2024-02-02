89df130e993c4f64b4f7ce29d84e11682013bbd4
Sourabh Sahu <104298270+sourabhsahu33@users.noreply.github.com>
2024-02-02 18:32:40 +0530

    Add files via upload

--- practice2.sql
+++ practice2.sql
@@ -12,8 +12,17 @@ INSERT INTO db_info(id, name, grade, marks, city)
 VALUES
 (01, "sourabh", "S", 99, "SEONI"),
 (02, "Ankit", "A", 95, 'Chhapra'),
-(03, "Chandan", "C", 90, "Chhapara"),
-(04, "Harshit", "H", 85, "Chhindwara");
+(03, "Chandan", "B", 90, "Chhapara"),
+(04, "Harshit", "C", 85, "Chhindwara"),
+(05,"vipin", "S", 99, "Jbp");
+
+INSERT INTO db_info value (06,"Manish", "A", 90, "CWA");
+INSERT INTO db_info value (08,"Neeraj", "D", 59, "ATA");
+SELECT DISTINCT city FROM db_info;
+SELECT grade , count(name)
+FROM db_info 
+group by grade order by grade desc;
+
 
 SELECT city, avg(marks)
 FROM db_info
@@ -24,6 +33,37 @@ SELECT name, avg(marks)
 FROM db_info
 GROUP BY name
 ORDER BY avg(marks) DESC;
+
+SELECT city, count(name)
+FROM db_info
+GROUP BY city
+HAVING max(marks)> 90;
+
+SELECT city, count(name)
+FROM db_info
+WHERE grade = "A"
+GROUP BY city
+HAVING max(marks) >= 80
+ORDER BY city ASC;
+
+SET SQL_SAFE_UPDATES = 0;
+-- UPDATE QUERIES 
+UPDATE db_info
+SET grade = "A"
+WHERE grade = "O";
+
+UPDATE db_info
+SET marks = 95
+where marks = 2;
+
+update db_info 
+set marks = marks+1;
+
+-- DELETE Queries
+DELETE FROM db_info
+WHERE marks < 70;
+
+DELETE FROM db_info;
 
 SELECT * FROM db_info;
 SELECT name 
